c42ff9481f44051bee08299a55ce673b414c4ab7
Davi <dnsbr7@gmail.com>
2022-11-05 00:43:54 -0300

    Aula 5 NLW Copa finalizada

---
+++ server/prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      avatarUrl: 'https://github.com/davi-souzadev.png',
+    }
+  })
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: 'Bolao',
+      code: 'bolao123',
+      ownerId: user.id,
+
+      participants: {
+        create: {
+          userId: user.id
+        }
+      }
+    }
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-01T23:28:39.657Z',
+      firstTeamCountryCode: 'DE',
+      secondTeamCountryCode: 'BR',
+    }
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-20T23:28:39.657Z',
+      firstTeamCountryCode: 'BR',
+      secondTeamCountryCode: 'AR',
+
+      guesses: {
+        create: {
+          firstTeamPoints: 2,
+          secondTeamPoints: 0,
+
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id
+              }
+            }
+          }
+        }
+      }
+    }
+  })
+}
+
+main()
